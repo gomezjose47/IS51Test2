d7ce965e0852525867993b43dce5b1b73d573966
Gomezjose47 <gomezjose47@mail.fresnostate.edu>
2020-12-03 16:50:18 -0800

    StructuredEnglish

---
+++ Test2.py
@@ -0,0 +1,18 @@
+"""
+The program will display the number of grades, the percentage of grades above the average grade, and the
+average grade of students on a final exam.
+There will be three functions in order to calculate the number of grades, the average grade, and the percentage of grades above the average grades.  
+Function1 will first count the number of grades and display the amount of grades there are in the txt file and display the number to the user. 
+Function2 will then find the average of the grades that are given in the txt file and display the average to the user
+Finally Function3 will calculate the percentage of grades that are above the average grade and display that percentage to the user. 
+There will be three functions to calculate the number, the average, and the percentage above the average
+
+Funtion1 will output the amount of grades that are in the Txt file
+Funtion2 will take add all the grades together and divide by the amount in function1 in order to get the average. 
+Function3 will then take that amount and count the number of exams that were above the average, then divide it by the average
+
+We then print out the Number of grades
+The Average grade
+and the Percentage of grades above the average grade. 
+
+"""
